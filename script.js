a74360e108d67b98b8fc1b570e79af8de37b1b03
Maurice Jaso <mauricejaso@outlook.com>
2018-09-11 16:49:42 +0200

    commit

--- script.js
+++ script.js
@@ -1,58 +1,94 @@
-
-var firstAndLast = [];
 var checkboxes = document.getElementsByTagName('input');
-//var checked = false;
-//console.log(checkboxes);
-//console.log(checkboxes[0]);
+var boxesChecked = [];
+var tempArray = [];
+var firstNum;
+var lastNum;
+var indexNum;
 
- for (var i = 0; i < checkboxes.length; i++) {
-   checkboxes[i].addEventListener('click', calculateRange);
- }
-
+for (var i = 0; i < checkboxes.length; i++) {
+  checkboxes[i].addEventListener('click', calculateRange);
+}
 
 function calculateRange () {
-var trueBoxes = [];
 
 	if (this.checked) {
-    //console.log('Checkbox ' + this.value + ' is True');
 
-    // Add all checked numbers to array
-    for (var i = 0; i < checkboxes.length; i++) {
+  // Empty arrays
+    tempArray = [];
+    boxesChecked = [];
+
+  // Add all checked numbers to array
+   	for (var i = 0; i < checkboxes.length; i++) {
     	if (checkboxes[i].checked) {
-      	trueBoxes.push(i);
+      	tempArray.push(i);
       }
     }
 
-    //console.log('trueBoxes ' + trueBoxes[0]);
-    //console.log('trueBoxes ' + trueBoxes[1]);
+  // Copy temporary array
+		boxesChecked = tempArray.slice();
 
-    // Add the first and last numbers to a new array
-    firstAndLast[0] = trueBoxes[0];
-    firstAndLast[1] = trueBoxes.pop();
+  // Add the first and last numbers to their respected variables
+    firstNum = tempArray.shift();
+    lastNum = tempArray.pop();
 
-		// Set checkboxes in range to true
-		for (var i = firstAndLast[0] + 1 ; i < firstAndLast[1]; i++) {
-    	//console.log(i);
-    	checkboxes[i].checked = true;
-      checkboxes[i].disabled = true;
+		if (boxesChecked.length > 1) {
+      // Set checkboxes within the range to true and disable them
+        for (var i = firstNum + 1; i < lastNum; i++) {
+          checkboxes[i].checked = true;
+          checkboxes[i].disabled = true;
+          if (boxesChecked.includes(checkboxes[i].value-1) == false) {
+          	boxesChecked.push(checkboxes[i].value-1);
+          }
+        }
+    	 boxesChecked.sort(function(a, b){return a-b});
+		}
+
+   // Display all elements in boxesChecked
+    for (var i = 0; i < boxesChecked.length; i++) {
+      console.log('boxesChecked ' + boxesChecked[i]);
     }
 
-    //checked = true;
-    //console.log('checked is: ' + checked);
   }
   else {
-  	console.log('Checkbox ' + this.value + ' is False');
+  		// Empty temporary array
+  	    tempArray = [];
 
-    // Set checkboxes in range to false
-		for (var i = firstAndLast[0] + 1; i < firstAndLast[1]; i++) {
-    	checkboxes[i].checked = false;
-      checkboxes[i].disabled = false;
-    }
+   	  // Create string array from number array
+  	    var arr = boxesChecked.slice();
+  	    var str = arr.toString();
+  	    var strArray = str.split(',');
 
-    //console.log('firstAndLast 0: ' + firstAndLast[0]);
-    //console.log('firstAndLast 1: ' + firstAndLast[1]);
+  	  // Get checkbox index number of unchecked box & convert it to string
+   	    var numValue = this.value - 1;
+  	    var strValue = numValue.toString();
 
-		//checked = false;
-    //console.log('checked is: ' + checked);
-  }
+  	  // Get index number of box that was unchecked from string array
+  	    indexNum = strArray.indexOf(strValue);
+
+   	  // Remove number that was unchecked from boxesChecked array
+  	    boxesChecked.splice(indexNum, 1);
+
+   	  // Copy new contents of boxesChecked array to the temporary array
+  	    tempArray = boxesChecked.slice();
+
+   	  // Add the first and last numbers to their respected variables
+  	    firstNum = tempArray.shift();
+  	    lastNum = tempArray.pop();
+
+   	  // Enable the checkboxes on the edges of the range
+  	    if (firstNum != undefined) {
+  	      checkboxes[firstNum].disabled = false;
+  	    }
+  	    if (lastNum != undefined) {
+  	      checkboxes[lastNum].disabled = false;
+  	    }
+
+  	   // Display all elements in boxesChecked
+  	    for (var i = 0; i < boxesChecked.length; i++) {
+  	      console.log('boxesChecked ' + boxesChecked[i]);
+  	    }
+
+  	  }
+
+
 }
